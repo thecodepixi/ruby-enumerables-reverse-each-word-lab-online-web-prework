76576c7a0e550184dbe3aed32afeb7a379691ca2
Emily Harber <emily.a.harber@gmail.com>
2019-08-18 17:38:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(words)
   
   word_array = words.split(" ")
   
-  reverse_sentence_words = word_array.each{ |x| binding.pry x.reverse }
+  reverse_sentence_words = word_array.each{ |x| console.log(x) }
   
   reversed_sentence_string = reverse_sentence_words.join(" ")
   
