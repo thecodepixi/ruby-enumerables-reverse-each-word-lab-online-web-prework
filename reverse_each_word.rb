9653a223e6914ba40885664593eb4db8e3fa7257
Emily Harber <emily.a.harber@gmail.com>
2019-08-18 17:49:19 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,28 @@
+def reverse_each_word(words) 
+  
+  word_array = words.split(" ")
+  
+  reverse_sentence_words = word_array.collect{ |x| x.reverse }
+  
+  reversed_sentence_string = reverse_sentence_words.join(" ")
+  
+  reversed_sentence_string
+  
+end
+
+# def reverse_each_word(words) 
+  
+#   word_array = words.split(" ")
+  
+#   reversed_array = []
+  
+#   word_array.each{|x| reversed_array.push(x.reverse)}
+  
+#   new_sentence = reversed_array.join(" ")
+  
+#   new_sentence
+  
+# end
+  
+  
+  
