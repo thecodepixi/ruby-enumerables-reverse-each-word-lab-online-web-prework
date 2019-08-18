a3b1fbc066e6e676bfc1a46f6e46c88c658230b2
Emily Harber <emily.a.harber@gmail.com>
2019-08-18 17:32:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ require "pry"
 def reverse_each_word(words) 
   
   binding.pry 
-  sentence_array = %w["#{words}"]
+  sentence_array = %w[#{words}]
   
   reverse_sentence_words = sentence_array.each{ |x| x.reverse }
   
